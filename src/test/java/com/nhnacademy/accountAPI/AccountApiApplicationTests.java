1c4e4f3dc1c4b0fe7a7cd836c5852836c12578c3
onyune <wjsdbwls0303@gmail.com>
2025-11-06 09:32:33 +0900

    init

---
+++ src/test/java/com/nhnacademy/accountAPI/AccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.accountAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
